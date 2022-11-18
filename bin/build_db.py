d0559ebf7830ef71399522c95957931235e44a21
Thijs Smolders <smolders.thijs@gmail.com>
2022-11-18 11:31:21 +0100

    refactor: black formatting of build_db and export_sktable executables

--- bin/build_db.py
+++ bin/build_db.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-#------------------------------------------------------------------------------#
+# ------------------------------------------------------------------------------#
 #  ParAutomatik: Automatic parametrisation of force fields                     #
 #  Copyright (C) 2019 - 2022  ParAutomatik developers group                             #
 #                                                                              #
 #  See the LICENSE file for terms of usage and distribution.                   #
-#------------------------------------------------------------------------------#
+# ------------------------------------------------------------------------------#
 
-'''Command line wrapper for the build_db script.'''
+"""Command line wrapper for the build_db script."""
 
 
 import sys
@@ -17,5 +17,5 @@ import ccs.scripts.ccs_build_db as ccs_build_db
 try:
     ccs_build_db.main()
 except ScriptError as exc:
-    sys.stderr.write('ScriptError: ' + str(exc) + '\n')
+    sys.stderr.write("ScriptError: " + str(exc) + "\n")
     sys.exit(1)
--- bin/export_sktable.py
+++ bin/export_sktable.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-#------------------------------------------------------------------------------#
+# ------------------------------------------------------------------------------#
 #  ParAutomatik: Automatic parametrisation of force fields                     #
 #  Copyright (C) 2019 - 2022  ParAutomatik developers group                             #
 #                                                                              #
 #  See the LICENSE file for terms of usage and distribution.                   #
-#------------------------------------------------------------------------------#
+# ------------------------------------------------------------------------------#
 
-'''Command line wrapper for the ccs_export_table script.'''
+"""Command line wrapper for the ccs_export_table script."""
 
 
 import sys
@@ -17,5 +17,5 @@ import ccs.scripts.ccs_export_sktable as ccs_export_sktable
 try:
     ccs_export_sktable.main()
 except ScriptError as exc:
-    sys.stderr.write('ScriptError: ' + str(exc) + '\n')
+    sys.stderr.write("ScriptError: " + str(exc) + "\n")
     sys.exit(1)
